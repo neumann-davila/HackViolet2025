7ad8455bc59e8a9b981d39745d2ac9cd3ef115bd
tiffanywid <tiffanywid@vt.edu>
2025-02-01 14:41:23 -0500

    sever working better

--- frontend/server.js
+++ frontend/server.js
@@ -1,25 +1,26 @@
-const WebSocket = require('ws');  // Import the ws library
-
-// Create a WebSocket server that listens on port 3000
+const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 3000 });
 
-wss.on('connection', (ws) => {
-  console.log('A user connected');
+wss.on("connection", (ws) => {
+  console.log("A user connected");
 
-  // When the server receives a message from the client
-  ws.on('message', (message) => {
-    const decodedMessage = message.toString();  // Convert buffer to string
-    console.log('Received message from client:', decodedMessage);  // Log readable message
+  ws.on("message", (message) => {
+    const decodedMessage = message.toString();
+    console.log("Received:", decodedMessage);
+
+    // Send message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(decodedMessage);
+      }
+    });
   });
 
-  // When the WebSocket connection is closed
-  ws.on('close', () => {
-    console.log('A user disconnected');
+  ws.on("close", () => {
+    console.log("A user disconnected");
   });
-
-  // Send a welcome message to the client when they connect
-  ws.send('Welcome to the WebSocket server!');
 });
 
-console.log('WebSocket server is running on ws://localhost:3000');
+console.log("WebSocket server running on ws://localhost:3000");
+
 
